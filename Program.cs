9d0eb70fe726c7b768548063e0f69fff015e7fb2
Ayoub-Medimegh <113118230+Ayoub-Medimegh@users.noreply.github.com>
2023-07-06 15:16:00 +0200

    Add files via upload
    "Celsius Fahrenheit Converter" is a simple console application that allows users to convert temperatures between Celsius and Fahrenheit. Users can choose to convert from Celsius to Fahrenheit or vice versa by selecting the appropriate option. The program prompts the user to enter a temperature value, performs the conversion, and displays the converted temperature. This project serves as a basic demonstration of temperature conversion using C# and provides a convenient tool for quick temperature conversions.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿float premiereTemperature = 0; // Variable to store the temperature entered by the user
+float resultat = 0; // Variable to store the conversion result
+
+Console.WriteLine("Press 1 to convert from Celsius to Fahrenheit. Press 2 to do the reverse.");
+
+ConsoleKeyInfo keyInfo; // Variable to store the user's keyboard input
+
+do
+{
+    keyInfo = Console.ReadKey(); // Read the key pressed by the user
+    Console.WriteLine();
+
+    if (keyInfo.KeyChar == '1')
+    {
+        Console.WriteLine("Enter a temperature in degrees Celsius to convert:");
+        premiereTemperature = Convert.ToSingle(Console.ReadLine()); // Convert the input to float
+        resultat = ((premiereTemperature * 9 / 5) + 32); // Perform the Celsius to Fahrenheit conversion
+        Console.WriteLine(premiereTemperature + "°C is equivalent to " + resultat + "°F.");
+    }
+
+    if (keyInfo.KeyChar == '2')
+    {
+        Console.WriteLine("Enter a temperature in degrees Fahrenheit to convert:");
+        premiereTemperature = Convert.ToSingle(Console.ReadLine()); // Convert the input to float
+        resultat = (premiereTemperature - 32) * 5 / 9; // Perform the Fahrenheit to Celsius conversion
+        Console.WriteLine(premiereTemperature + "°F is equivalent to " + resultat + "°C.");
+        Console.WriteLine("You can press 1 or 2 to perform another conversion, or press the Escape key to exit the program.");
+    }
+
+} while (keyInfo.Key != ConsoleKey.Escape); // Continue until the user presses the Escape key
